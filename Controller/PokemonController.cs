ea4163b46dfd1b7caffe8d7d420fe34dbe35fa1c
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-01-09 19:19:28 -0300

    feat: choose region

--- Controller/PokemonController.cs
+++ Controller/PokemonController.cs
@@ -1,6 +1,9 @@
 using Tamagotchi;
 using RestSharp;
 using System.Text.Json;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 
 
 namespace Pokemon
@@ -8,11 +11,51 @@ namespace Pokemon
     public class PokemonControl
     {
         public static PokemonModel? mascote;
+        public static string nickname = "";
         public static string GetPokemonName()
         {
             Console.Write("Digite o nome do pokemon: ");
             string pokemon = Console.ReadLine()!.ToLower();
             return pokemon;
+        }
+
+        public static string ChooseRegion()
+        {
+            Dictionary<int, string> regions = new Dictionary<int, string>()
+            {
+                {1, "Kanto"},
+                {2, "Johto"},
+                {3, "Hoenn"},
+                {4, "Sinnoh"},
+                {5, "Unova"},
+                {6, "Kalos"},
+            };
+
+            Console.WriteLine("Escolha uma região: ");
+            foreach (var item in regions)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            string region = Console.ReadLine()!;
+            return region;
+        }
+
+        public static string ChooseRegionInitial(string region)
+        {
+            region = region.ToLower();
+
+            Dictionary<string, string> regions = new Dictionary<string, string>()
+            {
+                {"kanto", "1 - Bulbasaur 2 - Squirtle 3 - Charmander"},
+                {"johto", "Johto"},
+                {"hoenn", "Hoenn"},
+                {4, "Sinnoh"},
+                {5, "Unova"},
+                {6, "Kalos"},
+            };
+
+            return regions[region];
         }
         public static async Task SearchPokemon(string pokemon)
         {
@@ -21,7 +64,7 @@ namespace Pokemon
             try
             {
                 Console.WriteLine($"Buscando pokemon: {pokemon}");
-                var request = new RestRequest($"pokemon/{pokemon}", Method.Get) {Timeout = 5000};
+                var request = new RestRequest($"pokemon/{pokemon}", Method.Get) { Timeout = 5000 };
                 var response = await client.GetAsync(request);
                 mascote = JsonSerializer.Deserialize<PokemonModel>(response.Content!);
                 Console.WriteLine("Pokemon encontrado!");
@@ -36,17 +79,14 @@ namespace Pokemon
 
         public static void ShowPokemon()
         {
-            Console.WriteLine($"Nome: {mascote?.Name?.ToUpper()}");
-            Console.WriteLine("Habilidades:");
-            foreach (var abilityEntry in mascote?.Abilities!)
-            {
-                Console.WriteLine($"- {abilityEntry?.Ability?.Name?.ToUpper()}");
-            }
-            Console.WriteLine("Status:");
-            foreach (var statsEntry in mascote?.Stats!)
-            {
-                Console.WriteLine($"- {statsEntry?.Stat?.Name?.ToUpper()} = {statsEntry?.BaseStat}");
-            }
+            if (nickname == "") Console.WriteLine($"Nome: {mascote?.Name?.ToUpper()}");
+            else Console.WriteLine($"Nome: {nickname} ({mascote?.Name?.ToUpper()})");
+
+            Console.WriteLine($""" 
+            Peso: {mascote!.Weight}
+            Altura: {mascote!.Height}
+            {mascote!.Stats![0].Stat!.Name!.ToUpper()}:  {mascote!.Stats![0].BaseStat}
+            """);
         }
 
 
