8900cc9ee00ae79f905d2f1cb2758f9f9761d1b2
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-01-19 18:20:09 -0300

    refactor: AdoptPet and ShowPet (name)

--- View/TamagotchiView.cs
+++ View/TamagotchiView.cs
@@ -61,10 +61,10 @@ namespace Pet
                 switch (choice)
                 {
                     case "1":
-                        await AdotarMascote();
+                        await AdoptPet();
                         break;
                     case "2":
-                        VerMascote();
+                        ShowPet();
                         break;
                     case "3":
                         Interact();
@@ -83,43 +83,48 @@ namespace Pet
             }
         }
 
-        public static async Task AdotarMascote()
+        public static async Task AdoptPet()
         {
             string pet;
+
             try
             {
-                // Selecionando a região
-                Console.WriteLine("Digite o nome da região:  ");
-                foreach (var item in PetController.regions) Console.WriteLine($"- {item[0].ToString().ToUpper()}{item[1..]}");
-
                 string region = GetRegion();
-
-                // Selecionando o pet
-                Console.WriteLine($"Iniciais de {region}:");
-                foreach (string starter in PetController.GetRegionStarter(region)) Console.WriteLine($"- {starter}");
-
-                pet = GetPokemon(region); ;
+                pet = GetPet(region); ;
             }
             catch (Exception err) { Console.WriteLine($"Erro: {err.Message}! Voltando ao início."); return; };
 
-            try
-            {
-                pet = pet.ToLower();
-                await PetController.SearchPet(pet);
-            }
-            catch (Exception err) { Console.WriteLine($"Erro: {err}!"); return; }
+            while (true) {
 
+                try
+                {
+                    pet = pet.ToLower();
+                    Console.WriteLine(pet);
+                    await PetController.SearchPet(pet);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine($"Erro: {err.Message}!");
+                    Console.Write("Continuar tentando? [s/n]");
+                    string ans = Console.ReadLine()!;
+
+                    if (ans == "s") continue;
+                    else return;
+                }
+            }
             Console.WriteLine($"Pet {pet} adotado com sucesso!");
 
             currentPet = PetController.currentPet!;
 
             Console.Write($"Qual o apelido do {pet}: ");
             currentPet.nickname = Console.ReadLine()!;
-
         }
 
         public static string GetRegion()
         {
+            Console.WriteLine("Digite o nome da região:  ");
+            foreach (var item in PetController.regions) Console.WriteLine($"- {item[0].ToString().ToUpper()}{item[1..]}");
+
             Console.Write(">> ");
             string region = Console.ReadLine()!.ToLower();
 
@@ -127,17 +132,21 @@ namespace Pet
             else throw new Exception("Região inválida");
         }
 
-        public static string GetPokemon(string region)
+        public static string GetPet(string region)
         {
-            Console.Write("Digite o nome do pokemon que você quer adotar: ");
-            string pokemon = Console.ReadLine()!;
+            Console.WriteLine($"Iniciais de {region}:");
+            foreach (string starter in PetController.GetRegionStarter(region))
+            {
+                Console.WriteLine($"- {starter[0].ToString().ToUpper()}{starter[1..]}");
+            }
+            Console.Write("Digite o nome do pet que você quer adotar: ");
+            string pet = Console.ReadLine()!.ToLower();
 
-            if (PetController.GetRegionStarter(region).Contains(pokemon!)) return pokemon;
-            else throw new Exception("Pokemon inválido");
+            if (PetController.GetRegionStarter(region).Contains(pet)) return pet;
+            else throw new Exception("Pet inválido");
         }
-        public static void VerMascote()
+        public static void ShowPet()
         {
-
             if (currentPet == null)
             {
                 Console.WriteLine("Você ainda não tem mascote!");
@@ -146,10 +155,15 @@ namespace Pet
 
             Console.WriteLine("Você pode ver todos os Pokemons adotados.");
 
-            if (currentPet.nickname == "") Console.WriteLine($"Nome: {currentPet.Name?.ToUpper()}");
-            else Console.WriteLine($"Nome: {currentPet.nickname} ({currentPet.Name?.ToUpper()})");
+            string nickname = currentPet!.nickname;
+            string species = currentPet.Name!;
 
+            //if (currentPet!.nickname == "") Console.WriteLine($"Nome: {currentPet.Name?.ToUpper()}");
+            //else Console.WriteLine($"Nome: {currentPet.nickname} ({currentPet.Name?.ToUpper()})");
+
+            string name = nickname != null ? $"{nickname} ({species})" : $"{species}";
             Console.WriteLine($""" 
+            Nome: {name}
             Vida:  {currentPet.Stats![0].BaseStat}
             Felicidade: {HappinesStatus(currentPet.happiness)} {currentPet.happiness}
             Anos de Vida: {currentPet.age}
