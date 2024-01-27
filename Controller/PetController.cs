c4db2765609d60f2fc43c7c951c8d4de368877a2
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-01-27 18:53:45 -0300

    chore: continued InteractionStatusUpdate, but not finished

--- Controller/PetController.cs
+++ Controller/PetController.cs
@@ -38,11 +38,8 @@ namespace Pet
         public static async Task SearchPet(string pet)
         {
             HttpClient client = new HttpClient();
-            Console.WriteLine("passou 1");
             string response = await client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{pet}");
-            Console.WriteLine("passou 2");
             currentPet = System.Text.Json.JsonSerializer.Deserialize<PetModel>(response);
-            Console.WriteLine("passou 3");
         }
 
         public static void CheckAndHandleDeath()
@@ -61,69 +58,38 @@ namespace Pet
             CheckAndHandleDeath();
 
             Random deprecation = new();
-            int deprecationFood = deprecation.Next(2, 10);
-            currentPet!.food -= deprecationFood;
+
+            currentPet.food -= deprecation.Next(2, 10);
             Math.Clamp(currentPet.food, 0, 100);
 
-            int deprecationHappinnes = deprecation.Next(2, 10);
-            currentPet.happiness -= deprecationHappinnes;
+            currentPet.happiness -= deprecation.Next(2, 10);
             Math.Clamp(currentPet.happiness, 0, 100);
 
-            currentPet.happiness += InteractionStatusUpdate(currentPet.happiness);
-            Math.Clamp(currentPet.happiness, 0, 100);
+            DeprecationStatusUpdate("food", {2, 10})
 
+            // Se eu conseguisse acessar um objeto através de uma string acho que daria certo
         }
 
-        public static int FeedPet()
+        public static int DeprecationStatusUpdate(string status, List<int> range)
         {
-            if (currentPet!.food > 80)
-            {
-                return -1;
-            }
-            else
-            {
-                int hunger = 100 - (currentPet!.food - 1);
-                Random food = new();
-
-                int foodSatisfaction = food.Next(0, hunger);
-                currentPet.food += foodSatisfaction;
-
-                return foodSatisfaction;
-            }
-        }
-
-        public static int PlayWithPet()
-        {
-            if (currentPet!.happiness > 80)
-            {
-                return -1;
-            }
-            else
-            {
-                int boredLevel = 100 - (currentPet!.happiness - 1);
-                Random random = new();
-
-                int happinessPoints = random.Next(0, boredLevel);
-                currentPet.happiness += happinessPoints;
-
-                return happinessPoints;
-            }
+            // Preciso dar um jeito de conseguir atualizar o status do pokemmon somente com essa função
+            Random deprecation = new();
+            int min = range[0]
+            int max = range[0]
+            currentPet.status -= deprecation.Next(min, max)
         }
 
         public static int InteractionStatusUpdate(int currentStatus)
         {
-            if (currentStatus > 80)
-            {
-                return -1;
-            }
+            // Preciso dar um jeito de conseguir atualizar o status do pokemmon somente com essa função
+            if (currentStatus > 80) return -1;
             else
             {
-                int differencePoints = 100 - (currentStatus - 1);
+                int rewardAmplitude = 100 - (currentStatus);
                 Random random = new();
 
-                int recoveryPoints = random.Next(0, differencePoints);
-
-                return recoveryPoints;
+                if (rewardExtent >= 10) random.Next(10, rewardExtent);
+                else 20;
             }
         }
 
